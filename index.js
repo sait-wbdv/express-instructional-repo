d8272492a7ca119b1eaf37279b41fc1604264874
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-15 12:53:42 -0600

    serve html files from pages dir

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 "use strict";
 
 const express = require("express");
-const logger = require("./logger");
 const path = require("node:path");
+const logger = require("./logger");
 const app = express();
 const port = 3000;
 
 app.use(logger);
 
 /* --- Routing code --- */
-app.get("/", (req, res) => {});
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "about.html"));
+});
 app.get("static-file", (req, res) => {});
 
 app.get("/api/numbers", (req, res) => {
