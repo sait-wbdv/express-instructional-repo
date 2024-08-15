566b7a3be4a7f17af6aa78d624d3c588079f767d
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-15 09:58:46 -0600

    simple server side examples

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello There, from Express Server");
 });
 
+app.get("/api/numbers", (req, res) => {
+  res.send([1, 2, 3, 4]);
+});
+
 app.listen(port, () => {
   console.log(`app is listening on http://localhost:${port}`);
 });
