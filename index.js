2d0a691602fcb60998c5b9903ad522097728f95b
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-15 11:08:25 -0600

    simple logger middleware example

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/* --- Middleware examples --- */
+
+// Custom logger
+function currentTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+function logger(req, res, next) {
+  console.log(`Time: ${currentTime()}`);
+  console.log(`method: ${req.method}`);
+  console.log(`path: ${req.originalUrl}`);
+  next();
+}
+
+app.use(logger);
+
+/* --- Routing code --- */
 app.get("/", (req, res) => {
   res.send("Hello There, from Express Server");
 });
